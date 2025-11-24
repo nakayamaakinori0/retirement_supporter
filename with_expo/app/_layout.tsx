6fc33b4de0d420b6ae7a6b90d9391f5a4a192d9b
Ak norinori <42559915+nakayamaakinori0@users.noreply.github.com>
2025-11-24 23:08:02 +0900

    format

--- with_expo/app/_layout.tsx
+++ with_expo/app/_layout.tsx
@@ -1,6 +1,6 @@
-import { Stack } from 'expo-router';
-import { RetirementDateProvider } from '../src/components/RetirementDateProvider';
-import { ModalProvider } from '../src/components/ModalProvider';
+import { Stack } from "expo-router";
+import { RetirementDateProvider } from "../src/components/RetirementDateProvider";
+import { ModalProvider } from "../src/components/ModalProvider";
 
 export default function RootLayout() {
   return (
@@ -8,13 +8,13 @@ export default function RootLayout() {
       <ModalProvider>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen 
-            name="retirement-day" 
-            options={{ 
-              title: '退職日',
+          <Stack.Screen
+            name="retirement-day"
+            options={{
+              title: "退職日",
               headerBackVisible: true,
-              headerBackTitle: '戻る',
-            }} 
+              headerBackTitle: "戻る",
+            }}
           />
         </Stack>
       </ModalProvider>
